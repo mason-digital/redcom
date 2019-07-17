cfae89c7cfe59dd2963fa17c157ebc549347e8f4
Chris Markham <chris@masondigital.com>
2019-07-17 16:21:33 -0400

    1.4

--- functions.php
+++ functions.php
@@ -28,3 +28,11 @@ require get_template_directory() . '/inc/admin_styles.php';
 
 
 require get_template_directory() . '/inc/redcom_walker.php';
+
+
+require 'plugin-update-checker/plugin-update-checker.php';
+$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+	'https://github.com/mason-digital/redcom',
+	__FILE__,
+	'redcom'
+);
